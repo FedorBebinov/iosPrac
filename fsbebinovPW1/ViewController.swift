5c6354ca63da2b04ffb04b503d2d825cf1827fe7
FedorBebinov <fsbebinov@edu.hse.ru>
2022-09-18 22:09:19 +0300

    Initial Commit

---
+++ fsbebinovPW1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fsbebinovPW1
+//
+//  Created by Fedor Bebinov on 18.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
